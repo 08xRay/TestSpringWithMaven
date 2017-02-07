82d534455c74324b43a139b53ff44760721afca2
Станислав Райцин <mr.steve.ray@gmail.com>
2017-02-07 10:39:01 +0300

    no message

--- src/main/java/GameManager.java
+++ src/main/java/GameManager.java
@@ -9,7 +9,7 @@ public class GameManager {
     }
 
     public static void fight(Character c1, Character c2) {
-        while (c1.isAlive() && c2.isAlive()){
+        while (c1.isAlive()){
             c1.kick(c2);
             System.out.println(c1.getClass().getSimpleName() + " " + c1.getHp() + "hp");
             System.out.println(c2.getClass().getSimpleName() + " " + c2.getHp() + "hp");
@@ -17,6 +17,8 @@ public class GameManager {
                 c2.kick(c1);
                 System.out.println(c1.getClass().getSimpleName() + " " + c1.getHp() + "hp");
                 System.out.println(c2.getClass().getSimpleName() + " " + c2.getHp() + "hp");
+            } else {
+                break;
             }
         }
     }
